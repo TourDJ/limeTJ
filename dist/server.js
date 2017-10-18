471348f49693416a96ef57745fd3ad31d77853c4
TourDJ <tangxiaojun_123@126.com>
2017-10-18 21:58:07 +0800

    initial config

--- dist/server.js
+++ dist/server.js
@@ -21,7 +21,7 @@ const MongoStore = mongo(session);
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.config({ path: ".env.example" });
+dotenv.config({ path: ".env" });
 /**
  * Controllers (route handlers).
  */
--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ const MongoStore = mongo(session);
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.config({ path: ".env.example" });
+dotenv.config({ path: ".env" });
 
 
 /**
